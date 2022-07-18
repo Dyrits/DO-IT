28f10f3d5d312da10eaa21ec63f6d833e8825c92
Dylan J. Gerrits <dyrits@outlook.com>
2022-07-18 15:34:57 +0000

    PATCH endpoint added.

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -5,19 +5,26 @@ export let todos: Todo[] = [];
 
 export const api = async ({ locals, request, params }: RequestEvent) => {
     let response: RequestHandlerOutput = { status: 500 };
+    let data: FormData;
+    const uid = params?.uid;
     // @ts-ignore
     switch (locals.method) {
         case "GET":
             response = { ...response, status: 200, body: todos };
             break;
         case "POST":
-            const data = await request.formData();
-            const todo = data.get("text")?.toString();
-            todo && todos.push({ uid: `${Date.now()}`, text: todo, done: false, created_at: new Date() });
-            response = { ...response, body: todo };
+            data = await request.formData();
+            const text = data.get("text")?.toString();
+            text && todos.push({ uid: `${Date.now()}`, text, done: false, created_at: new Date() });
+            response = { ...response, body: text };
+            break;
+        case "PATCH":
+            data = await request.formData();
+            const todo = todos.find(todo => todo.uid === uid);
+            // @ts-ignore
+            response = { ...response, body: (todo.text = data.get("text")?.toString()) };
             break;
         case "DELETE":
-            const { uid } = params;
             todos = todos.filter(todo => todo.uid !== uid);
             break;
         default:
--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from "@sveltejs/kit";
 
 import { api } from "./_api";
 
-export const get: RequestHandler = (event) => { return api(event); }
+export const get: RequestHandler = async (event) => { return await api(event); }
 export const post: RequestHandler = async (event) => { return await api(event); }
